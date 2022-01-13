86e99914d8ed4a432b3bda323a5401804f96d76e
Jake Gilfix <jakemgilfix@gmail.com>
2022-01-12 16:49:12 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        paleblue: 'hsl(225, 100%, 94%)',
+        brightblue: 'hsl(245, 75%, 52%)',
+        verypaleblue: 'hsl(225, 100%, 98%)',
+        desaturatedblue: 'hsl(224, 23%, 55%)',
+        darkblue: 'hsl(223, 47%, 23%)',
+        activeblue: 'hsl(245, 55%, 95%)',
+      },
+      fontFamily: {
+        sans: ['Red Hat Display', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
